22a415d50350466a7b43776ebcf1de7907f30e70
吴洛祥（实习） <wuluoxiang@xiaohongshu.com>
2023-10-03 23:56:58 +0800

    update main.go

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"my_test/cache"
 	"my_test/db"
 	"my_test/kafka"
@@ -8,10 +10,16 @@ import (
 )
 
 func main() {
-	db.InitConfig()
+
+	http_port := flag.String("http_port", "8081", "http port")
+	external := flag.Bool("external", false, "use external kafka host:port")
+	group_id := flag.String("group_id", "0", "kafka consumer group id")
+	flag.Parse()
+
+	db.InitConfig(*external)
 	db.InitLog()
 	cache.InitGCache()
 	kafka.InitKafkaCfg()
-	go server.StartConsumerServer()
-	server.StartHttpServer()
+	go server.StartConsumerServer(context.Background(), *group_id)
+	server.StartHttpServer(*http_port)
 }
